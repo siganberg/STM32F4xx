6d7619812bbbe3d9fd5a92983c2a7a5f2fb96a11
siganberg <francis.marasigan@gmail.com>
2025-01-01 15:38:46 -0800

    This version is specific for SLB only.

--- extra_script.py
+++ extra_script.py
@@ -1,9 +1,14 @@
 Import("env")
+
+env.Replace(PROGNAME="%s" % env.GetProjectOption("custom_prog_version")+"_"+env.GetProjectOption("grblhal_driver_version"))
+env.Replace(custom_board_name="%s" % env.GetProjectOption("custom_prog_version")+"_"+env.GetProjectOption("grblhal_driver_version"))
+
 # Custom HEX from ELF
 env.AddPostAction(
     "$BUILD_DIR/${PROGNAME}.elf",
     env.VerboseAction(" ".join([
-        "$OBJCOPY", "-O", "ihex", "-R", ".eeprom",
-        "$BUILD_DIR/${PROGNAME}.elf", "$BUILD_DIR/${PROGNAME}.hex"
+        "$OBJCOPY", "-O", "ihex", "-R", ".eeprom", 
+        '"$BUILD_DIR/${PROGNAME}.elf"', '"$BUILD_DIR/${PROGNAME}.hex"'
     ]), "Building $BUILD_DIR/${PROGNAME}.hex")
 )
+
